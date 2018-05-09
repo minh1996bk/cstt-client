d2af84c22af8d5555e410c4ede390a3910c8ede8
minh1996bk <minh1996bk@gmail.com>
2018-05-09 08:37:36 +0700

    fix loi text

--- migrate.js
+++ migrate.js
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
 
 connection.connect();
 // sua bang benh
-connection.query("alter table `result` modify `solution` varchar(255) character set utf8 collate utf8_vietnamese_ci;", function(err, res) {
+connection.query("alter table `result` modify `solution` text character set utf8 collate utf8_vietnamese_ci;", function(err, res) {
     if (err) return console.error(err);
     else {
         console.log("1 column modified");
